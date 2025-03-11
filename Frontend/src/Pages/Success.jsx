e0cb21fdd97e033f6c0ee8223ac460569bbca11c
Ayesha_Nazar <ayesh.nazar@plcgroup.com>
2025-03-11 07:15:07 +0500

    Home page:: hero  section

---
+++ Frontend/src/Pages/Success.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+      <>
+          <h1>Success page</h1>
+      
+      </>
+      
+   
+      
+  )
+}
+
+export default Success
